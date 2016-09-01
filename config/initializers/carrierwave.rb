e864d9e0736d79aedfbcb724722dac6635b1ec96
Carl-Philip <callea2@gmail.com>
2016-09-01 16:45:51 +0200

    Deployment path fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
   if Rails.env.test? || Rails.env.cucumber?
     config.storage = :file
     config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
+  # config.root = "#{Rails.root}/tmp"
   else
     config.storage = :aws
   end
